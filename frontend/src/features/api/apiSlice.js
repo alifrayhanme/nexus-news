4c4391ad20509fb82b20e8a9ed30ee4300b3690f
Alif Rayhan <alifrayhanme@gmail.com>
2025-10-31 17:14:14 +0600

    try fix deploy issue

--- frontend/src/features/api/apiSlice.js
+++ frontend/src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: import.meta.env.VITE_API_URL || "https://backend-59p1m1khf-alifrayhans-projects.vercel.app",
+    baseUrl: import.meta.env.VITE_API_URL || "https://nexus-news-opal.vercel.app",
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
       const token = localStorage.getItem("accessToken");
