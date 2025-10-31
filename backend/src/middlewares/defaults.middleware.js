f8371add1e9993d5ce6bbb3a0957af14613f443d
Alif Rayhan <alifrayhanme@gmail.com>
2025-10-31 17:08:50 +0600

    fix try  deploy  issue

--- backend/src/middlewares/defaults.middleware.js
+++ backend/src/middlewares/defaults.middleware.js
@@ -9,7 +9,11 @@ const cors = require("cors");
 module.exports = function (app) {
   app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: [
+        "http://localhost:5173",
+        "http://localhost:3000", 
+        /^https:\/\/.*\.vercel\.app$/
+      ],
       credentials: true,
     })
   );
